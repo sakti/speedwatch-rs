238d2df6b11eefc4b4cfb734056d74adf1c297c2
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2025-08-17 21:19:37 +0700

    initial commit
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use std::{str::FromStr, time::Duration};
+
+use base64::{Engine, engine::general_purpose::STANDARD};
+use cfspeedtest::{
+    OutputFormat,
+    speedtest::{run_latency_test, test_download},
+};
+use clap::Parser;
+use miette::{IntoDiagnostic, Result, miette};
+use prometheus_remote_write::{LABEL_NAME, Label, Sample, TimeSeries, WriteRequest};
+use reqwest::blocking::Client;
+
+const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about,
+    long_about = "speedwatch will monitor your internet speed and latency and push it to prometheus remote write"
+)]
+struct Args {
+    /// Interval in minutes
+    #[arg(short, long, default_value_t = 30)]
+    interval: u64,
+
+    /// Remote write URL
+    #[arg(short, long, env = "SW_REMOTE_WRITE_URL", default_value_t = String::from("http://localhost:9090/api/v1/write"))]
+    remote_write_url: String,
+
+    /// Remote write username
+    #[arg(short, long, env = "SW_REMOTE_WRITE_USERNAME")]
+    username_remote_write: String,
+
+    /// Remote write password
+    #[arg(short, long, env = "SW_REMOTE_WRITE_PASSWORD")]
+    password_remote_write: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let hostname = hostname::get().into_diagnostic()?;
+
+    println!("Hello {}!", args.interval);
+    println!("speedwatch-rs");
+    println!("Testing download speed with 10MB of payload");
+
+    let download_speed = test_download(
+        &reqwest::blocking::Client::new(),
+        10_000_000,
+        OutputFormat::None, // don't write to stdout while running the test
+    );
+
+    println!("download speed in mbit: {download_speed}");
+
+    let (_, avg_latency) = run_latency_test(
+        &reqwest::blocking::Client::new(),
+        25,
+        OutputFormat::None, // don't write to stdout while running the test
+    );
+    println!("average latency in ms: {avg_latency}");
+
+    // build write requests
+    let time: i64 = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .into_diagnostic()?
+        .as_millis()
+        .try_into()
+        .into_diagnostic()?;
+
+    let mut bw_labels: Vec<Label> = vec![];
+    bw_labels.push(Label {
+        name: "hostname".to_string(),
+        value: hostname.to_string_lossy().into_owned(),
+    });
+    bw_labels.push(Label {
+        name: LABEL_NAME.to_string(),
+        value: "sw_internet_bandwidth_mbit".to_string(),
+    });
+    let bw_timeseries = TimeSeries {
+        labels: bw_labels,
+        samples: vec![Sample {
+            value: download_speed,
+            timestamp: time,
+        }],
+    };
+
+    let mut latency_labels: Vec<Label> = vec![];
+    latency_labels.push(Label {
+        name: "hostname".to_string(),
+        value: hostname.to_string_lossy().into_owned(),
+    });
+    latency_labels.push(Label {
+        name: LABEL_NAME.to_string(),
+        value: "sw_internet_latency_ms".to_string(),
+    });
+    let latency_timeseries = TimeSeries {
+        labels: latency_labels,
+        samples: vec![Sample {
+            value: avg_latency,
+            timestamp: time,
+        }],
+    };
+
+    let write_request = WriteRequest {
+        timeseries: vec![bw_timeseries, latency_timeseries],
+    };
+
+    let mut req = write_request
+        .build_http_request(
+            &args
+                .remote_write_url
+                .parse::<url::Url>()
+                .into_diagnostic()?,
+            USER_AGENT,
+        )
+        .map_err(|err| miette!("operation failed: {}", err))?;
+
+    let credentials = STANDARD.encode(format!(
+        "{}:{}",
+        args.username_remote_write, args.password_remote_write
+    ));
+    req.headers_mut().insert(
+        "Authorization",
+        format!("Basic {}", credentials).parse().unwrap(),
+    );
+
+    // send the http::Request
+    let client = Client::builder()
+        .timeout(Duration::from_secs(30))
+        .build()
+        .into_diagnostic()?;
+
+    let (parts, body) = req.into_parts();
+    let method = reqwest::Method::from_str(parts.method.as_str()).into_diagnostic()?;
+    let mut req_builder = client.request(method, parts.uri.to_string());
+    for (name, value) in parts.headers.iter() {
+        req_builder = req_builder.header(name.to_string(), value.as_bytes());
+    }
+    req_builder = req_builder.body(body);
+    let response = req_builder.send().into_diagnostic()?;
+
+    dbg!(response);
+    // send the metric
+    dbg!(time);
+    dbg!(hostname);
+    Ok(())
+}
